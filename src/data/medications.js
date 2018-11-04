e587f869bcef4eeb6b255dd0f81f0633453c2a94
Jonathan Mast <jon@jonmast.com>
2018-11-04 07:17:16 -0500

    Add patient info page with medications list

---
+++ src/data/medications.js
@@ -0,0 +1,104 @@
+const medications = [
+  {
+    key: 1,
+    medication: "Atorvastatin 20mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/5/12",
+    pickup_date: "12/17/12",
+    pharmacy: "CVS #8650"
+  },
+  {
+    key: 2,
+    medication: "Metoprolol succinate 25mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/10/12",
+    pickup_date: "12/17/12",
+    pharmacy: "CVS #8650"
+  },
+  {
+    key: 3,
+    medication: "Atorvastatin 20mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/5/12",
+    pickup_date: "11/20/12",
+    pharmacy: "Walgreens #1932"
+  },
+  {
+    key: 4,
+    medication: "Metoprolol succinate 25mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/14/12",
+    pickup_date: "11/20/12",
+    pharmacy: "Walgreens #1932"
+  },
+  {
+    key: 5,
+    medication: "Clopidogrel 75mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/14/12",
+    pickup_date: "11/20/12",
+    pharmacy: "Walgreens #1932"
+  },
+  {
+    key: 6,
+    medication: "Aspirin 325mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/14/12",
+    pickup_date: "11/20/12",
+    pharmacy: "Walgreens #1932"
+  },
+  {
+    key: 7,
+    medication: "Atorvastatin 20mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "11/5/12",
+    pickup_date: "11/5/12",
+    pharmacy: "CVS #8650"
+  },
+  {
+    key: 8,
+    medication: "Hydrochlorothiazide 25mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "1/16/12",
+    pickup_date: "11/11/12",
+    pharmacy: "CVS #8650"
+  },
+  {
+    key: 9,
+    medication: "Atorvastatin 10mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "1/16/12",
+    pickup_date: "9/10/12",
+    pharmacy: "Rite Aid #4321"
+  },
+  {
+    key: 10,
+    medication: "Hydrochlorothiazide 25mg",
+    sig: "1 PO daily",
+    qty: "30",
+    days_supply: "30",
+    rx_date: "1/16/12",
+    pickup_date: "9/10/12",
+    pharmacy: "Rite Aid #4321"
+  }
+];
+
+export default medications;
